5385c3b2ab7cfa6d612464687da2a97da2cfa2cc
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2021-01-29 18:22:12 +0200

    Task completed

--- app.py
+++ app.py
@@ -1,3 +1,95 @@
+from flask import (
+    Flask,
+    session,
+    escape,
+    render_template,
+    request,
+    url_for,
+    redirect,
+    flash,
+)
 import data
 
-# put your code here
+app = Flask(__name__)
+app.secret_key = "iqwr87fgbwisfv0w/akjc^"
+
+
+@app.route("/")
+def index():
+    user = None
+    if "username" in session:
+        user = escape(session["username"])
+    return render_template("index.html", user=user)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if username in data.users.keys():
+            if data.verify_password(password, data.users[username]):
+                session["username"] = request.form["username"]
+                return redirect(url_for("index"))
+            else:
+                flash("Wrong username or password")
+        else:
+            flash("Wrong username or password")
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # remove the username from the session if it's there
+    session.pop("username", None)
+    return redirect(url_for("index"))
+
+
+current_question = 0
+results = 0
+
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    if "username" not in session:
+        return redirect(url_for("index"))
+
+    global current_question
+    global results
+
+    questions = list(data.questions.keys())
+
+    if request.method == "POST":
+        answer = request.form["answer"]
+        current_question += 1
+        
+        if str(answer) == "True":
+            results += 1
+
+    if current_question == len(questions):
+        return redirect("/result")
+
+    question = questions[current_question]
+    answers = []
+
+    for v in data.questions[question]:
+        answers.append(
+            {
+                "question": v,
+                "value": data.questions[question][v],
+            }
+        )
+
+    return render_template("test.html", question=question, answers=answers)
+
+
+@app.route("/result")
+def result():
+    if "username" not in session:
+        return redirect(url_for("index"))
+
+    return render_template("result.html", results=results)
+
+
+if __name__ == "__main__":
+    app.run()
--- data.py
+++ data.py
@@ -1,35 +1,49 @@
+import bcrypt
+
 # TODO: add your users and hashed passwords here
-users = {'john@doe.com': '$2b$12$/TYFvXOy9wDQUOn5SKgTzedwiqB6cm.UIfPewBnz0kUQeK9Eu4mSC'}
+# users = {'john@doe.com': '$2b$12$/TYFvXOy9wDQUOn5SKgTzedwiqB6cm.UIfPewBnz0kUQeK9Eu4mSC'}
+users = {"user@user.me": "$2b$12$PlYJF7hckz/DAAsg6d.h2.lj.ACcUQvwTfKbapeRCzuuMx9r8c1km"}
 
 questions = {
     "I ______ bus on Mondays.": {
         "a. 'm going to work with": False,
         "b. 'm going to work by": False,
         "c. go to work with": False,
-        "d. go to work by": True
+        "d. go to work by": True,
     },
     "Sorry, but this chair is ______.": {
         "a. me": False,
         "b. mine": True,
         "c. my": False,
-        "d. our": False
+        "d. our": False,
     },
     "A: 'How old ______?'   B: 'I ______ .'": {
         "a. are you / am 20 years old.": True,
         "b. have you / have 20 years old.": False,
         "c. are you / am 20 years.": False,
-        "d. do you have / have 20 years.": False
+        "d. do you have / have 20 years.": False,
     },
     "I ______ to the cinema.": {
         "a. usually don't go": False,
         "b. don't usually go": True,
         "c. don't go usually": False,
-        "d. do not go usually": False
+        "d. do not go usually": False,
     },
     "Where ______ ?": {
         "a. your sister works": False,
         "b. your sister work": False,
         "c. does your sister work": True,
-        "d. do your sister work": False
-    }
+        "d. do your sister work": False,
+    },
 }
+
+
+def hash_password(plain_text_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode("utf-8"), bcrypt.gensalt())
+    return hashed_bytes.decode("utf-8")
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode("utf-8")
+    return bcrypt.checkpw(plain_text_password.encode("utf-8"), hashed_bytes_password)
